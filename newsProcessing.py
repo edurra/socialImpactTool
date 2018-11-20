306615957752993cf46b8eccfb42c6834bdc6d5e
javiermorenosan <javiermsm12@gmail.com>
2018-11-19 19:28:07 -0600

    Final processing scripts

--- newsProcessing.py
+++ newsProcessing.py
@@ -16,7 +16,7 @@ sqlContext = sql.SQLContext(sc)
 
 stopwords = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "chars", "into", "through","like", "get", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "doesn", "didn", "shouldn", "wouldn", "couldn", "even", "just", "don", "should", "now", "could", "would"]
 
-news = sc.textFile('/user/maria_dev/sampleNews.csv')
+news = sc.textFile('/user/maria_dev/tempNews.csv')
 
 news_split = news.map(lambda l: l.split("\t"))
 
@@ -27,6 +27,8 @@ stopwords.append(word_searched)
 news_types = news_split.map(lambda line: [line[1], line[2], int(line[3]), line[4], line[5]])
 
 news_title_date = news_types.map(lambda line: [line[0], line[1]])
+
+news_title_source = news_types.map(lambda line: [line[0], line[3]])
 
 just_text = news_types.map(lambda line: [line[0], line[4]])
 
@@ -61,6 +63,29 @@ predictions_without_text = predictions_rdd.map(lambda line: [line[0], line[2]])
 news_with_prediction = predictions_without_text.join(news_title_date)
 
 news_with_prediction = news_with_prediction.map(lambda line: [line[0], line[1][0], line[1][1]])
+
+news_sources_prediction = news_with_prediction.join(news_title_source)
+
+news_sources_prediction_iterable = news_sources_prediction.map(lambda line: [line[1][1], line[1][0]]).groupByKey()
+news_sources_prediction = news_sources_prediction_iterable.map(lambda line: [line[0], list(line[1])])
+
+negative_sources_1 = news_sources_prediction.map(lambda line: [[x for x in line[1] if x == 0.0], line[0]])
+negative_sources = sc.parallelize(negative_sources_1.map(lambda line: [len(line[0]), line[1]]).sortByKey(False).take(5))
+
+positive_sources = sc.parallelize(news_sources_prediction.map(lambda line: [float(sum(line[1])), line[0]]).sortByKey(False).take(5))
+print "A"
+print "A"
+print "A"
+print "A"
+print "A"
+print "A"
+print "A"
+print "A"
+print "A"
+print "A"
+print "A"
+print positive_sources.take(5)
+print negative_sources.take(5)
 
 news_with_prediction_date_first = news_with_prediction.map(lambda line: [line[2], line[1]])
 
@@ -232,6 +257,10 @@ print "4"
 print "4"
 print words_ordered_content_percentage.take(15)
 
+#Number of news per day
+news_per_day_iterable = news_split.map(lambda line: [line[2], line[3]]).groupByKey()
+news_per_day = news_per_day_iterable.map(lambda line: [line[0], list(line[1])[0]])
+
 #MySQL
 
 #positive and negative news
@@ -239,16 +268,15 @@ pos_neg_per_day_searchId = pos_neg_per_day.map(lambda line: [searchId,line[1], l
 fields = [StructField('searchId', IntegerType(), True),StructField('newsPosPercentage', DoubleType(), True), StructField('newsNegPercentage', DoubleType(), True), StructField('date', StringType(), True)]
 schema = StructType(fields)
 newsPosNegDf = sqlContext.createDataFrame(pos_neg_per_day_searchId , schema)
-newsPosNegDf.write.format('jdbc').options(url = 'jdbc:mysqlx', driver='com.mysql.jdbc.Driver', dbtable='newsDaily', user='x', password='x').mode('append').save()
+newsPosNegDf.write.format('jdbc').options(url = 'jdbc:mysql://XXX.XXX.XXX.XXX:XXXX/socialImpact', driver='com.mysql.jdbc.Driver', dbtable='newsDaily', user='X', password='X').mode('append').save()
 
 #words in content
-
 words_content_searchId = words_ordered_content_percentage.map(lambda line: [searchId, line[1], line[0]]).take(10)
 words_content_searchId_rdd = sc.parallelize(words_content_searchId)
 fields = [StructField('searchId', IntegerType(), True),StructField('word', StringType(), True), StructField('weight', DoubleType(), True)]
 schema = StructType(fields)
 words_content_searchId_df = sqlContext.createDataFrame(words_content_searchId_rdd , schema)
-words_content_searchId_df.write.format('jdbc').options(url = 'jdbc:mysqlx', driver='com.mysql.jdbc.Driver', dbtable='wordsContents', user='x', password='x').mode('append').save()
+words_content_searchId_df.write.format('jdbc').options(url = 'jdbc:mysql://XXX.XXX.XXX.XXX:XXXX/socialImpact', driver='com.mysql.jdbc.Driver', dbtable='wordsContents', user='X', password='X').mode('append').save()
 
 #words in title
 words_title_searchId = words_ordered_title_percentage.map(lambda line: [searchId, line[1], line[0]]).take(10)
@@ -256,4 +284,25 @@ words_title_searchId_rdd = sc.parallelize(words_title_searchId)
 fields = [StructField('searchId', IntegerType(), True),StructField('word', StringType(), True), StructField('weight', DoubleType(), True)]
 schema = StructType(fields)
 words_title_searchId_df = sqlContext.createDataFrame(words_title_searchId_rdd , schema)
-words_title_searchId_df.write.format('jdbc').options(url = 'jdbc:mysql://x', driver='com.mysql.jdbc.Driver', dbtable='wordsTitles', user='x', password='x').mode('append').save()
+words_title_searchId_df.write.format('jdbc').options(url = 'jdbc:mysql://XXX.XXX.XXX.XXX:XXXX/socialImpact', driver='com.mysql.jdbc.Driver', dbtable='wordsTitles', user='X', password='X').mode('append').save()
+
+#positive sources
+positive_sources_rdd = positive_sources.map(lambda line: [searchId, line[1]])
+fields = [StructField('searchId', IntegerType(), True), StructField('sources', StringType(), True)]
+schema = StructType(fields)
+positive_sources_df = sqlContext.createDataFrame(positive_sources_rdd , schema)
+positive_sources_df.write.format('jdbc').options(url = 'jdbc:mysql://XXX.XXX.XXX.XXX:XXXX/socialImpact', driver='com.mysql.jdbc.Driver', dbtable='newsPosSources', user='X', password='X').mode('append').save()
+
+#negative sources
+negative_sources_rdd = negative_sources.map(lambda line: [searchId, line[1]])
+fields = [StructField('searchId', IntegerType(), True), StructField('sources', StringType(), True)]
+schema = StructType(fields)
+negative_sources_df = sqlContext.createDataFrame(negative_sources_rdd , schema)
+negative_sources_df.write.format('jdbc').options(url = 'jdbc:mysql://XXX.XXX.XXX.XXX:XXXX/socialImpact', driver='com.mysql.jdbc.Driver', dbtable='newsNegSources', user='X', password='X').mode('append').save()
+
+#News per day
+news_per_day_rdd = news_per_day.map(lambda line: [searchId, int(line[1]), line[0]])
+fields = [StructField('searchId', IntegerType(), True), StructField('results', IntegerType(), True), StructField('date', StringType(), True)]
+schema = StructType(fields)
+news_per_day_df = sqlContext.createDataFrame(news_per_day_rdd , schema)
+news_per_day_df.write.format('jdbc').options(url = 'jdbc:mysql://XXX.XXX.XXX.XXX:XXXX/socialImpact', driver='com.mysql.jdbc.Driver', dbtable='newsResultsPerDay', user='X', password='X').mode('append').save()
